73b7ea2b673f30d7ca75eaac57105abf27190025
Zhenya Hitrova <jarotea@yandex.ru>
2024-07-21 23:52:16 +1000

    Some styles in progress

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,13 @@
 import MenuMain from "../../pages/home/MenuMain";
 import Logo from "../ui/logoUI/Logo";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <>
+    <div className={styles.header}>
       <Logo />
       <MenuMain />
-    </>
+    </div>
   );
 };
 
