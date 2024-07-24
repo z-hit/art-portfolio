efc5442fed9a2e7b28eb065ad13d30633ef9a7c9
Zhenya Hitrova <jarotea@yandex.ru>
2024-07-24 23:36:59 +1000

    Home page ready

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,17 @@
+import styles from "./Home.module.css";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className={styles.image_box}>
+      <img
+        src={require("../../assets/images/Home-cover.JPG")}
+        className={styles.image}
+        alt="Acrylic painting of urquoise magic fish with chicken legs and angel wings"
+        width="300"
+        height="auto"
+      />{" "}
+    </div>
+  );
 };
 
 export default Home;
