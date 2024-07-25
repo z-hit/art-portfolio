4986e2c0a820ff01d393c936d83b609aef2e4272
Zhenya Hitrova <jarotea@yandex.ru>
2024-07-25 22:58:01 +1000

    Edited assets

--- src/pages/gallery/ArtProject.tsx
+++ src/pages/gallery/ArtProject.tsx
@@ -1,15 +1,3 @@
-import { useParams } from "react-router-dom";
-import { artworksData } from "../../data/artworks";
-import Page404 from "../page404/Page404";
-import { TArtwork } from "../../components/ui/galleryUI/GalleryUI";
-import { useEffect, useState } from "react";
-
 export const ArtProject = () => {
-  const { project } = useParams();
-  const { year } = useParams();
-  const [artworks, setArtworks] = useState([]);
-
-  /* useEffect(); */
-
-  return <p>yes</p>;
+  return <p>hello</p>;
 };
