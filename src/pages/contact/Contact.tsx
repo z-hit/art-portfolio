e7b7758e025ca811e44f0fe02f3e592dc9f2a378
Zhenya Hitrova <jarotea@yandex.ru>
2024-07-25 15:38:47 +1000

    Contact improved layout

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
 import styles from "./Contact.module.css";
+import clsx from "clsx";
 
 const Contact = () => {
   return (
     <div className={styles.section}>
-      <div className={styles.text_box}>
+      <div className={clsx(styles.text_box, styles.right)}>
         <h3 className={styles.section_title}>Email</h3>
         <p className={styles.text}>hitrovazhenya@gmail.com</p>
       </div>
       <div className={styles.text_box}>
         <h3 className={styles.section_title}>Social media</h3>
         <div className={styles.social_link_box}>
-          <h4 className={styles.social_title}>Instagram:</h4>
+          <h4 className={styles.social_title}>Instagram: </h4>
           <Link
             to={"https://instagram.com/zhenya_hitrova"}
             className={styles.link}
@@ -20,13 +21,13 @@ const Contact = () => {
           </Link>
         </div>
         <div className={styles.social_link_box}>
-          <h4 className={styles.social_title}>VK:</h4>
+          <h4 className={styles.social_title}>VK: </h4>
           <Link to={"https://vk.ru/catasian"} className={styles.link}>
-            Художник Женя Хитрова
+            @Художник Женя Хитрова
           </Link>
         </div>
         <div className={styles.social_link_box}>
-          <h4 className={styles.social_title}>WeChat:</h4>
+          <h4 className={styles.social_title}>WeChat: </h4>
           <Link to={""} className={styles.link}>
             @jarotea
           </Link>
