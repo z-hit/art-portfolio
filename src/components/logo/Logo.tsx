17756a7035768a00a3ed2e8ee7d26f8c1d0f2105
Zhenya Hitrova <jarotea@yandex.ru>
2024-09-05 22:35:03 +1000

    Logo stamp added

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,7 +2,18 @@ import { Link } from "react-router-dom";
 import styles from "./LogoUI.module.css";
 
 const Logo = () => {
-  return <Link to={'/'} className={styles.text}>Zhenya Hitrova</Link>;
+  return (
+    <Link to={"/"} className={styles.logo_box}>
+      <img
+        className={styles.logo}
+        src={require("../../assets/images/logo.jpg")}
+        alt="artins logo aignature"
+        width="100"
+        height="auto"
+      />
+      <p className={styles.text}>Zhenya Hitrova</p>
+    </Link>
+  );
 };
 
 export default Logo;
