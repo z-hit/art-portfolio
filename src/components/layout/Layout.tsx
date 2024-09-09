25fea088ce31cb21fc1d7d6a90bedafcb6aa2dd5
Zhenya Hitrova <jarotea@yandex.ru>
2024-09-09 22:47:44 +1000

    css vars started

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
+import style from "./Layout.module.css";
 
 const Layout = () => {
   return (
