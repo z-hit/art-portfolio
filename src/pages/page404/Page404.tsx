c1c34dc6d1051d332f22da0dfdfa85e5a9749ed5
Zhenya Hitrova <jarotea@yandex.ru>
2024-09-27 00:56:04 +1000

    fix: Page 404 design improved

--- src/pages/page404/Page404.tsx
+++ src/pages/page404/Page404.tsx
@@ -1,5 +1,14 @@
+import styles from "./Page404.module.css";
+
 const Page404 = () => {
-  return <h1>404 Page Not Found</h1>;
+  return (
+    <div className={styles.text_box}>
+      <h1 className={styles.title}>404 Page Not Found</h1>;
+      <p className={styles.text}>
+        Please, visit any existing pages from the site menu above.
+      </p>
+    </div>
+  );
 };
 
 export default Page404;
